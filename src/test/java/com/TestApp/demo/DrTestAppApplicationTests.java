fe05b5647618f4be1a0095fed059881519eda80d
Prasad15587 <65469116+Prasad15587@users.noreply.github.com>
2020-05-17 23:47:58 +0200

    Initial Files

---
+++ src/test/java/com/TestApp/demo/DrTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
